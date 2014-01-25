8002827275d76b5b6641aba1a967132f3e6064fb
M477h3w1012 <m.477h3w@live.co.uk>
2014-01-25 18:06:17 +0000

    And I forgot to put in that startup change... clumsy me.

--- sql/startup.sql
+++ sql/startup.sql
@@ -148,7 +148,7 @@ CREATE TABLE `@users`(
   `primary_group_id` integer(11) not null default '3',
   `rank_id` integer(11) not null default '1',
   `user_email` varchar(128) not null,
-  `password` varchar(40) not null,
+  `password` varchar(64) not null,
   `time_reg` integer(11) not null,
   `time_pass_altered` integer(11) not null,
   `user_timezone` decimal(5,2) not null,
